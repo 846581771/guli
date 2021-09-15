5d7c2c16514f17edbf59f9cc029d6371968e5c3a
yuanmingLiu <yli5@iu.edu>
2021-09-14 21:55:36 -0400

    project initial structure creation.

---
+++ order/src/main/java/com/yuanmingliu/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.yuanmingliu.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
